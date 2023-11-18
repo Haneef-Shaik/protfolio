11a18be96f1ef657928fef43e57193e4c6e8d363
Haneef-Shaik <shaikhaneef123a@gmail.com>
2023-11-18 06:48:04 +0530

    scroll speed

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,29 @@ import Skills from './screens/Skills';
 function App() {
   const scrollRef = useRef(null);
 
-  useEffect(() => {
-    const scrollingDiv = scrollRef.current;
-    const handleScroll = (event) => {
-      event.preventDefault();
-      const scrollAmount = 0.95; // Adjust this value for your desired scroll speed
+  // useEffect(() => {
+  //   const scrollingDiv = scrollRef.current;
+  //   const handleScroll = (event) => {
+  //     event.preventDefault();
+  //     const scrollAmount = 0.95; // Adjust this value for your desired scroll speed
 
-      // Calculate the amount to scroll
-      const scrollIncrement = event.deltaY * scrollAmount;
+  //     // Calculate the amount to scroll
+  //     const scrollIncrement = event.deltaY * scrollAmount;
 
-      // Animate the scroll
-      scrollingDiv.scrollTop += scrollIncrement;
-    };
+  //     // Animate the scroll
+  //     scrollingDiv.scrollTop += scrollIncrement;
+  //   };
 
-    if (scrollingDiv) {
-      scrollingDiv.addEventListener("wheel", handleScroll, { passive: false });
-    }
+  //   if (scrollingDiv) {
+  //     scrollingDiv.addEventListener("wheel", handleScroll, { passive: false });
+  //   }
 
-    return () => {
-      if (scrollingDiv) {
-        scrollingDiv.removeEventListener("wheel", handleScroll);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (scrollingDiv) {
+  //       scrollingDiv.removeEventListener("wheel", handleScroll);
+  //     }
+  //   };
+  // }, []);
   return (
     <div className='mainPage w-screen h-screen overflow-auto scrollbar-hide bg-[#262626]' ref={scrollRef}  >
       <Landing/> 
